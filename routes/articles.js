787ee6396ad91b33ec36f0f2fb8b8643fef00bf7
lokita282 <lokitavarma282@gmail.com>
2021-06-16 16:47:41 +0530

    final proj

--- routes/articles.js
+++ routes/articles.js
@@ -6,24 +6,52 @@ router.get('/new', (req, res) => {
   res.render('articles/new', {article: new Article()})
 } )
 
-router.get('/:id', (req, res) => {
+router.get('/edit/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  res.render('articles/edit', {article})
+} )
 
-})
+router.get('/new/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  res.render('articles/edit', {article})
+} )
 
-router.post('/', async (req , res) => {
-  const article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown
-  })
-  try{
-    article = await article.save()
-    res.redirect(`/articles/${article.id}`)
-  } catch (e) {
-    res.render('articles/new', {article: article})
+router.get('/:slug', async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug },)
+  if(!article){
+    res.redirect('/')
   }
-
-  
+  res.render('articles/show', {article})
 })
+
+router.post('/', async (req , res, next) => {
+  req.article = new Article()
+  next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req , res, next) => {
+  req.article = await Article.findById(req.params.id)
+  next()
+}, saveArticleAndRedirect('edit'))
+
+router.delete('/:id', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/')
+})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+      article.title = req.body.title
+      article.description = req.body.description
+      article.markdown = req.body.markdown
+    try{
+      article = await article.save()
+      res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+      res.render(`articles/${path}`, {article})
+    }
+  }
+} 
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const article = require('./models/article')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect(("mongodb+srv://lokita282:abcd1234@cluster0.voagm.mongodb.net/blog?retryWrites=true&w=majority"), {
@@ -11,19 +14,11 @@ mongoose.connect(("mongodb+srv://lokita282:abcd1234@cluster0.voagm.mongodb.net/b
 })
 
 app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  const articles = [{ 
-    title: 'test article',
-    createdAt : new Date(),
-    description: 'Test description'
-  }, {
-    title: 'test article 2',
-    createdAt : new Date(),
-    description: 'Test description 2'
-  }]
+app.get('/', async (req, res) => {
+  const articles = await article.find().sort({createdAt: 'desc'})
   res.render('articles/index', {articles})
 })
 
